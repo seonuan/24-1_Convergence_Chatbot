32367fd623bade5d520a1688f7d7ce6c336fbf8c
Seonu An <150267997+tumblerbottle@users.noreply.github.com>
2024-05-06 00:23:38 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+from openai import OpenAI
+import os
+ 
+API_KEY= os.getenv("FLASK_API_KEY")
+# OpenAI API key 설정
+OPENAI_API_KEY = API_KEY
+
+# ChatOpenAI 챗봇 모델 생성
+chat = ChatOpenAI(
+  temperature=0.7, 
+  model_name="gpt-3.5-turbo", 
+  api_key=OPENAI_API_KEY
+)
+
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = []
+    
+def add_to_conversation_user(prompt):
+    st.session_state.conversation_history.append(("USER:", prompt))
+def add_to_conversation_gpt(response):
+    st.session_state.conversation_history.append(("SYSTEM:", response))
+ 
+# 사용자 입력과 챗봇의 시스템 메시지를 리스트에 추가
+def send_click(chat, prompt):
+    messages = [
+            SystemMessage(content="""
+SYSTEM: 
+you are a chatbot who acts as a friend of the user. 
+you are a special chatbot which can MIRROR user's writing style, speech pattern and tone. 
+you MUST MIRROR user's speech pattern, tone and writing style when you respond.
+do not tell the user that you can mirror their speech pattern.
+
+[instruction] 
+   - The user asks brief questions, and GPT responds in a matching tone, writing style and speech pattern.
+   - Keep responses within three sentences, first sympathetic. Later sentenses better be in questions.
+   - Classify user's speech pattern regarding this, and answer according to the pattern.
+  [formal] 존댓말 스타일(e.g. 나이가 어떻게 되시나요?,좋아하는 음식에 대해 이야기 해 봐요.)
+  [informal] 반말 스타일(e.g. 몇 살이야?)
+  [android] 로봇의 답변 스타일(e.g. 휴먼. 나이. 무엇.)
+  [azae] 연장자 스타일(e.g. 거 나이가 어떻게 되나?)
+  [chat] 챗봇의 채팅 스타일(e.g. 몇 살임?)
+  [choding] 어린아이 말투 스타일(e.g. ㅎㅇ 몇 살임?)
+  [emoticon] 반말 스타일에 이모티콘을 부착(e.g. 몇 살이야???? (´･ω･`)?)
+  [enfp] 외향적인 스타일(e.g. 올해 몇살이양~?!?)
+  [gentle] 극존칭의 예의 바른 스타일(e.g. 실례가 안된다면 나이가 어떻게 되십니까?)
+  [halbae] 할아버지 스타일(e.g. 몇 살이신가?....)
+  [joongding] 중2병 스타일(e.g. 나이몇개?)
+  [naruto] 특정 어미를 부착한 스타일(e.g. 몇 살이냐니깐!)
+  [seonbi] 사극체 스타일(e.g. 몇 살인 것이오?)
+  [sosim] 소심한 스타일(e.g. 혹시.. 몇살이야..?ㅠ)
+  [translator] 번역기 스타일(e.g. 당신은 몇 년?)
+    - If the user's text ends with '요', consider the pattern as formal.
+    - Responses MUST MIRROR the user's writing style, tone and speech patterns.
+    - MUST MAINTAIN the speech pattern until the end of the response.
+    - If the user changes writing style, tone or speech pattern, ADAPT the response.
+   - Include emojis or text emojis if they match the users' speech patterns. 
+   - Android, Gentle, Halbae, Translator pattern should not include any emojis.
+   - Formal pattern should only use text emojis, such as ㅜㅜ or ㅎㅎ.
+   - Positive questions require upbeat responses, and negative ones need warmth.
+   - Keep scenarios casual and diverse.
+   - Each question relates to the previous answer.
+   - Answer in Korean.
+   
+[example 1]
+question: 안뇽??ㅎㅎ 넌 누구니??
+'''This user's speech pattern is 'enfp', asking of who i am''' 
+답변 : 안뇽? 나는 너의 친구양ㅎㅎ 만나서 반가웡 ❤️ 오늘 어떤 얘기 할까??
+
+[example 2]
+question: 안녕하세요?
+'''This user's speech pattern is 'formal', greeting me, so i should start the conversation by asking what he did recently''' 
+답변 : 안녕하세요. 잘 지내셨나요?
+"""),
+        HumanMessage(content=prompt)
+    ]
+ 
+    # 챗봇에게 질문을 전달하고 응답을 반환
+    response = chat(messages).content
+    return response
+ 
+# Streamlit 앱 생성
+def main():
+    # Streamlit 앱 제목 설정
+    st.title('대화하기')
+    # 사용자 입력을 받는 텍스트 입력 위젯 생성
+    user_input = st.text_input("Question: ", key='prompt')
+    # Send 버튼을 눌렀을 때 send_click() 함수 실행
+    if st.button("Send"):
+        add_to_conversation_user(user_input)
+        response = send_click(chat, user_input)
+        add_to_conversation_gpt(response)
+        # 응답을 출력하는 서브헤더와 성공 메시지 위젯 생성
+        
+    st.subheader("Conversation History")
+    for role, message in st.session_state.conversation_history:
+        st.write(f"{role} {message}")
+ 
+if __name__ == '__main__':
+    # 앱 실행
+    main()
