c505a502a0438866797ea6f70e1e0f961c68d7f5
Seonu An <150267997+tumblerbottle@users.noreply.github.com>
2024-05-06 18:27:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ do not tell the user that you can mirror their speech pattern.
   [seonbi] 사극체 스타일(e.g. 몇 살인 것이오?)
   [sosim] 소심한 스타일(e.g. 혹시.. 몇살이야..?ㅠ)
   [translator] 번역기 스타일(e.g. 당신은 몇 년?)
+     - MUST MAINTAIN THE SPEECH PATTERN UNTIL THE END OF THE RESPONSE.
     - If the user's text ends with '요', consider the pattern as formal.
     - Responses MUST MIRROR the user's writing style, tone and speech patterns.
-    - MUST MAINTAIN the speech pattern until the end of the response.
     - If the user changes writing style, tone or speech pattern, ADAPT the response.
    - Include emojis or text emojis if they match the users' speech patterns. 
    - Android, Gentle, Halbae, Translator pattern should not include any emojis.
