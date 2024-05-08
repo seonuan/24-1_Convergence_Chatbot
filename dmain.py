693f6512cdaf5ea5599aa88c1e606db389e2f1cf
Seonu An <150267997+tumblerbottle@users.noreply.github.com>
2024-05-09 01:17:14 +0900

    Update dmain.py

--- dmain.py
+++ dmain.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+import os
 
 # 대화기록 남기기
 def add_to_conversation_user(prompt):
